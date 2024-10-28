ec76680697ea261e4274016deee6357e61ae58a4
jhanslarico11 <jhanslarico11@gmail.com>
2024-10-28 11:03:07 -0500

    PROCRUD

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Application;
 trait CreatesApplication
 {
     /**
-     * Creates the application.
+     * Creates the application.anyelo
      */
     public function createApplication(): Application
     {
